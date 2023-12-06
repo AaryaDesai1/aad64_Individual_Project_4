ebf88a38dd6cdfcae27170f2157ba1f8ba1a68c8
AaryaDesai1 <aad64@duke.edu>
2023-12-06 16:48:48 -0500

    Adding test file

---
+++ test_flaskapp.py
@@ -0,0 +1,41 @@
+import unittest
+from flask import Flask
+from flask.testing import FlaskClient
+from flask.wrappers import Response
+
+from flask_app import app
+
+
+class FlaskAppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app
+        self.client = self.app.test_client()
+
+    def test_home_route(self):
+        response: Response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Home", response.data)
+
+    def test_traits_route(self):
+        response: Response = self.client.get("/traits")
+        self.assertEqual(response.status_code, 200)
+
+    def test_predict_route(self):
+        response: Response = self.client.post(
+            "/predict", data={"prompt": "programming"}
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(b"Redirecting", response.data)
+
+    def test_result_route(self):
+        response: Response = self.client.get("/result")
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_top_professions(self):
+        traits = "programming"
+        result = app.get_top_professions(traits)
+        self.assertIsInstance(result, str)
+
+
+if __name__ == "__main__":
+    unittest.main()
